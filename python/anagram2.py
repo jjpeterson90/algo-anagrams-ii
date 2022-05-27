22f4a46141a0feb4006fe0a72438e33ba799e2cc
Justin <jjpeterson90@hotmail.com>
2022-05-27 16:27:20 -0500

    complete

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,16 @@
 # Don't forget to run the tests (and create some of your own)
 
 def anagrams_for(word, list_of_words):
-		# your code here
+	anagrams_found = []
+	for this_word in list_of_words:
+		test_this_word = list(this_word)
+		for letter in word:
+			if letter in test_this_word:
+					test_this_word.remove(letter)
+		if len(test_this_word) == 0:
+				anagrams_found.append(this_word)
+	return anagrams_found
+
+
+#list_of_words = ["threads", "trashed", "hardest", "hatreds", "hounds"]
+#print(anagrams_for("threads", list_of_words))
