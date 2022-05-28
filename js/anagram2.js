026aab7a3fd87d335de00841fb501c907fb47572
Justin <jjpeterson90@hotmail.com>
2022-05-28 16:07:09 -0500

    complete

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,18 @@
 exports.anagramsFor = function(word, listOfWords) {
-
+    let anagramsFound = [];
+    word = word.split('');
+    listOfWords.forEach(thisWord => {
+        if (thisWord.length == word.length) {
+            var testThisWord = thisWord.split('');
+            for (let i = 0; i < thisWord.split('').length; i++) {
+                letter = thisWord[i];
+                //console.log(letter)
+                if (word.includes(letter)) {
+                    testThisWord.splice(testThisWord.indexOf(letter), 1);
+                }
+            }
+            if (testThisWord.length == 0) anagramsFound.push(thisWord);
+        }
+    })
+    return anagramsFound
 };
